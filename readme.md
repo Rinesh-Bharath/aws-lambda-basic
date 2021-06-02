a9f4659356b73d134415c4732ddcdc517172df0d
Rinesh <rinesh93bharath@gmail.com>
2021-06-02 20:45:38 +0530

    Updated input JSON

--- readme.md
+++ readme.md
@@ -16,6 +16,39 @@
 }
 ```
 
+2.
+
+```
+{
+	"action": "read_product",
+	"body": {
+		"input": {
+			"product_id": "85034f29-8324-48b9-b617-5ca408e03d9f"
+		}
+	}
+}
+```
+
+```
+{
+	"service": "api-product",
+	"data": {
+		"_id": "6090058e78fc3d5858a739ee",
+		"product_id": "85034f29-8324-48b9-b617-5ca408e03d9f",
+		"status": "INACTIVE",
+		"name": "Chicken Pizza",
+		"price": 160,
+		"color": "Yellow",
+		"audit": {
+			"created_by": "f4a276e5-f470-4f85-b0ec-a48c5c380a72",
+			"created_at": "2021-05-03T14:15:40.962Z",
+			"updated_by": "f4a276e5-f470-4f85-b0ec-a48c5c380a72",
+			"updated_at": "2021-05-03T14:15:40.962Z"
+		}
+	}
+}
+```
+
 ## run instructions
 
 npm i
--- src/helper/product/read.js
+++ src/helper/product/read.js
@@ -3,11 +3,11 @@ import { fetch_one_from_db } from '../../../shared/mongodb';
 export async function read (logging_key, filter, return_value) {
   try {
     console.log(`${logging_key} - started - ${JSON.stringify(filter)}`);
-    const FEED = await fetch_one_from_db(logging_key, process.env.TABLE_FEED, filter);
-    if (!FEED) {
-      throw Error('FEED does not exists');
+    const PRODUCT = await fetch_one_from_db(logging_key, process.env.TABLE_PRODUCT, filter);
+    if (!PRODUCT) {
+      throw Error('PRODUCT does not exists');
     }
-    return_value['data'] = FEED;
+    return_value['data'] = PRODUCT;
     console.log(`${logging_key} - finished - ${JSON.stringify(return_value)}`);
     return return_value;
   } catch (error) {
